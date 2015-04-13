095ec53805acb61124aea4c83680b80201316996
Michael Genovesi <michaelgenovesi@gmail.com>
2015-04-13 17:36:21 -0600

    all but bonus working

--- main.js
+++ main.js
@@ -13,25 +13,43 @@ $(".topo").on("click", function(event){
 	console.log(y);
 
 	var pin = $('<img class="pin" src="pin-locator.png">');
+	var note = $('<textarea class="note">Enter a note here</textarea>');
 
 $('.container').append(pin);
+$('.container').append(note);
 	
   	$(pin)
   		.css('position','absolute')
   		.css('top', y)
   		.css('left', x)
-  		.show();
+  		.show()
+  		.on("click",function(){
+  			$(this).hide();
+  		});
+
+  	 $(note)
+  		.css('position','absolute')
+  		.css('top', y+30)
+  		.css('left', x+30)
+  		.show()
+  		.on("blur",function(){
+  			var userNote = $(this).val();
+  			$(this).hide();
+  			console.log(userNote);
+  		});
+
+
+
 
   });
 
+//Doesn't work.  Just testing out the hover effect
 
+// var testBox = $('<div class="div-box"></div>');
 
-
-
-
-
-
-
+// $(pin).on("hover",function(){
+// 	$(testBox).show();
+// });
 
   
 });
